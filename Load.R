0fc80b5b2fec849eb39519546e19c863215d0c03
Kristian Brock <brockk@users.noreply.github.com>
2022-12-14 09:43:28 +0000

    Abandoned attempt for script to download data from repo automatically.
    Script now assumes that data is in Excel file in executing dir called Database.xlsx

--- Load.R
+++ Load.R
@@ -14,10 +14,10 @@ library(here)
 # v1.2:
 # data_url <- 'http://edata.bham.ac.uk/337/3/Database_v1.2.xlsx'
 # v1.3:
-data_url <- 'http://edata.bham.ac.uk/337/4/Database_v1.3.xlsx'
-GET(data_url, write_disk(file_loc <- tempfile(fileext = ".xlsx")))
+# data_url <- 'http://edata.bham.ac.uk/337/4/Database_v1.3.xlsx'
+# GET(data_url, write_disk(file_loc <- tempfile(fileext = ".xlsx")))
 # Or load locally (n.b. change path to suit):
-# file_loc <- here('..', 'Database.xlsx')
+file_loc <- "Database.xlsx"
 
 # Load datasets:
 manuscripts <- read_excel(file_loc, sheet = 'Manuscripts')
